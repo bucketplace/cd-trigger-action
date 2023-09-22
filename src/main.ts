4dc10dd3dd6bb5e08748727586e0dc5a5b09d3b8
aiden.lim <aiden.lim@bucketplace.net>
2023-09-22 16:05:14 +0900

    fix ci error

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ async function run(): Promise<void> {
     const imageTag: string = core.getInput('image-tag', {required: true})
     const version: string = core.getInput('version', {required: true})
     const env: string = core.getInput('env')
-    const commitSha: string | undefined = core.getInput('commit-sha');
-    const repoUrl: string | undefined = core.getInput('repo-url');
+    const commitSha: string | undefined = core.getInput('commit-sha')
+    const repoUrl: string | undefined = core.getInput('repo-url')
 
     core.debug(
       `Trigger CD for ${application}, profile: ${profile}, imageTag: ${imageTag}, version: ${version}`
