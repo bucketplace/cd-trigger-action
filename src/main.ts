bed0e2fb2de4e3578a9bc5c46f433ae3c2e05bf4
aiden.lim <aiden.lim@bucketplace.net>
2023-09-22 15:35:29 +0900

    new inputs: commit-sha, repo-url

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ async function run(): Promise<void> {
     const imageTag: string = core.getInput('image-tag', {required: true})
     const version: string = core.getInput('version', {required: true})
     const env: string = core.getInput('env')
+    const commitSha: string | undefined = core.getInput('commit-sha');
+    const repoUrl: string | undefined = core.getInput('repo-url');
 
     core.debug(
       `Trigger CD for ${application}, profile: ${profile}, imageTag: ${imageTag}, version: ${version}`
@@ -16,7 +18,11 @@ async function run(): Promise<void> {
       profile,
       image_tag: imageTag,
       version,
-      env: env ? env : undefined
+      env: env ? env : undefined,
+      extra: {
+        commit_sha: commitSha ? commitSha : undefined,
+        repo_url: repoUrl ? repoUrl : undefined
+      }
     })
   } catch (error) {
     core.setFailed(error.message)
